e8f864f51520c77cd760dc9b00da7c0a04e1b07a
Mohammed-Awadallah <dr.mohammed.j.awadallah@gmail.com>
2022-06-23 04:18:28 +0300

    updated the footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -12,7 +12,7 @@ export default function App() {
     <MDBFooter className='bg-dark text-center text-white'>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
-          <a className='btn btn-outline-light btn-floating m-1' href='#!' role='button'>
+          <a className='btn btn-outline-light btn-floating m-1' href='dr.mohammed.j.awadallah@gmail.com' role='button'>
             <MDBIcon fab icon='google' />
           </a>
           <a className='btn btn-outline-light btn-floating m-1' href='https://www.instagram.com/hxh_jazz/' role='button'>
@@ -30,9 +30,9 @@ export default function App() {
       </MDBContainer>
 
       <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2020 Copyright:
-        <a className='text-white' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+        © 2022 Copyright:
+        <a className='text-white' href='https://www.instagram.com/asac.ltuc/?hl=en'>
+          ASAC: The Searchers
         </a>
       </div>
     </MDBFooter>
